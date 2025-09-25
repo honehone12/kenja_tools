38778e60700aec514e6e92d2fc970d0406fdb212
Twelfthonehone <honehone.41games@gmail.com>
2025-09-25 12:18:18 +0900

    fixed api chara

--- src/bin/api_chara.rs
+++ src/bin/api_chara.rs
@@ -1,12 +1,12 @@
-use std::{env, time::Duration};
+use clap::Parser;
 use futures::TryStreamExt;
-use tokio::time;
+use kenja_tools::{api::request, documents::anime_src::AniCharaBridge};
 use mongodb::{bson::doc, Client as MongoClient};
 use reqwest::Client as HttpClient;
-use clap::Parser;
 use serde_json::Value;
+use std::{env, time::Duration};
+use tokio::time;
 use tracing::{info, warn};
-use kenja_tools::{documents::anime_src::AniCharaBridge, api::request};
 
 #[derive(Parser)]
 #[command(version)]
@@ -14,38 +14,43 @@ struct Args {
     #[arg(long, default_value_t = 1500)]
     interval_mil: u64,
     #[arg(long, default_value_t = 10000)]
-    timeout_mil: u64
+    timeout_mil: u64,
 }
 
 async fn req_chara(
     args: Args,
     mongo_client: MongoClient,
-    http_client: HttpClient
+    http_client: HttpClient,
 ) -> anyhow::Result<()> {
     let src_db = mongo_client.database(&env::var("API_SRC_DB")?);
     let src_cl = src_db.collection::<AniCharaBridge>(&env::var("API_SRC_CL")?);
-    
-    let dst_db = mongo_client.database(&env::var("API_DST_DB")?);  
+
+    let dst_db = mongo_client.database(&env::var("API_DST_DB")?);
     let dst_cl = dst_db.collection::<Value>(&env::var("API_DST_CL")?);
-    
+
     let interval = Duration::from_millis(args.interval_mil);
     let timeout = Duration::from_millis(args.timeout_mil);
 
     let base_url = env::var("BASE_API_URL")?;
-    info!("getting flat url list");
 
-    let done = dst_cl.distinct("mal_id", doc! {}).await?.iter()
+    let done = dst_cl
+        .distinct("mal_id", doc! {})
+        .await?
+        .iter()
         .filter_map(|bson| bson.as_i64())
         .collect::<Vec<i64>>();
 
-    let list = src_cl.find(doc! {}).await?
-        .try_collect::<Vec<AniCharaBridge>>().await?;
+    let list = src_cl
+        .find(doc! {})
+        .await?
+        .try_collect::<Vec<AniCharaBridge>>()
+        .await?;
     let mut total = 0;
-    
+
     // this stream should be cached
     for bridge in list {
         let mut batch = vec![];
-        
+
         for chara_cast in bridge.characters {
             if done.contains(&chara_cast.character.mal_id) {
                 continue;
@@ -66,7 +71,10 @@ async fn req_chara(
 
         if !batch.is_empty() {
             let res = dst_cl.insert_many(&batch).await?;
-            info!("iteration {total}. inserted {} items", res.inserted_ids.len());
+            info!(
+                "iteration {total}. inserted {} items",
+                res.inserted_ids.len()
+            );
         }
     }
 
@@ -84,6 +92,6 @@ async fn main() -> anyhow::Result<()> {
     let mongo_client = MongoClient::with_uri_str(mongo_uri).await?;
 
     let http_client = HttpClient::new();
-    
+
     req_chara(args, mongo_client, http_client).await
 }
